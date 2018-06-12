e7a9e10b774bdbf7edd76c291088852a3f190977
Serge Kushner <kusnir.sergiu@gmail.com>
2018-06-12 17:26:35 +0200

    second commit

---
+++ ReadingXML/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ReadingXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ReadXMLButton = new System.Windows.Forms.Button();
+            this.ShowSchemaButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AuthorsDataSet = new System.Data.DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AuthorsDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(264, 289);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ReadXMLButton
+            // 
+            this.ReadXMLButton.Location = new System.Drawing.Point(462, 359);
+            this.ReadXMLButton.Name = "ReadXMLButton";
+            this.ReadXMLButton.Size = new System.Drawing.Size(147, 23);
+            this.ReadXMLButton.TabIndex = 2;
+            this.ReadXMLButton.Text = "ReadXML";
+            this.ReadXMLButton.UseVisualStyleBackColor = true;
+            this.ReadXMLButton.Click += new System.EventHandler(this.ReadXMLButton_Click);
+            // 
+            // ShowSchemaButton
+            // 
+            this.ShowSchemaButton.Location = new System.Drawing.Point(88, 359);
+            this.ShowSchemaButton.Name = "ShowSchemaButton";
+            this.ShowSchemaButton.Size = new System.Drawing.Size(110, 23);
+            this.ShowSchemaButton.TabIndex = 3;
+            this.ShowSchemaButton.Text = "ShowSchema";
+            this.ShowSchemaButton.UseVisualStyleBackColor = true;
+            this.ShowSchemaButton.Click += new System.EventHandler(this.ShowSchemaButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(316, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(438, 289);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // AuthorsDataSet
+            // 
+            this.AuthorsDataSet.DataSetName = "AuthorsDataSet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShowSchemaButton);
+            this.Controls.Add(this.ReadXMLButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AuthorsDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ReadXMLButton;
+        private System.Windows.Forms.Button ShowSchemaButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Data.DataSet AuthorsDataSet;
+    }
+}
+
---
+++ ReadingXML/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadingXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadXMLButton_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\seraph\source\repos\ReadingXML\ReadingXML\authors.xml";
+
+            DataSet ds = new DataSet();
+            ds.ReadXml(filePath);
+            dataGridView1.DataSource = ds.Tables[0];
+           
+            //AuthorsDataSet.ReadXml(filePath);
+            //dataGridView1.DataSource = AuthorsDataSet;
+            //dataGridView1.DataMember = "authors";
+        }
+
+        private void ShowSchemaButton_Click(object sender, EventArgs e)
+        {
+            System.IO.StringWriter swXML = new System.IO.StringWriter();
+            AuthorsDataSet.WriteXmlSchema(swXML);
+            textBox1.Text = swXML.ToString();
+        }
+    }
+}
